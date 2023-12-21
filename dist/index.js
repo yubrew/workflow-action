df0677a88c30210ff4d34df2725a8c824198af6e
yubrew <john@chaindefender.ai>
2023-12-21 18:29:59 -0500

    refactored main

--- dist/index.js
+++ dist/index.js
@@ -28912,56 +28912,27 @@ function wrappy (fn, cb) {
 const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 async function run() {
   try {
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
     const owner = core.getInput('owner', { required: true })
     const repo = core.getInput('repo', { required: true })
-    const pr_number = core.getInput('pr_number', { required: true })
+    const prNumber = core.getInput('pr_number', { required: true })
     const token = core.getInput('token', { required: true })
 
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
     const octokit = new github.getOctokit(token)
 
-    /**
-     * We need to fetch the list of files that were changes in the Pull Request
-     * and store them in a variable.
-     * We use octokit.paginate() to automatically loop over all the pages of the
-     * results.
-     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-     */
     const { data: changedFiles } = await octokit.rest.pulls.listFiles({
       owner,
       repo,
-      pull_number: pr_number
+      pull_number: prNumber
     })
 
-    /**
-     * Contains the sum of all the additions, deletions, and changes
-     * in all the files in the Pull Request.
-     **/
     let diffData = {
       additions: 0,
       deletions: 0,
       changes: 0
     }
 
-    // Reference for how to use Array.reduce():
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
     diffData = changedFiles.reduce((acc, file) => {
       acc.additions += file.additions
       acc.deletions += file.deletions
@@ -28969,21 +28940,14 @@ async function run() {
       return acc
     }, diffData)
 
-    /**
-     * Loop over all the files changed in the PR and add labels according
-     * to files types.
-     **/
     for (const file of changedFiles) {
-      /**
-       * Add labels according to file types.
-       */
       const fileExtension = file.filename.split('.').pop()
       switch (fileExtension) {
         case 'md':
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['markdown']
           })
           break
@@ -28991,23 +28955,16 @@ async function run() {
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['javascript']
           })
           break
         case 'yml':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['yaml']
-          })
-          break
         case 'yaml':
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['yaml']
           })
           break
@@ -29015,7 +28972,7 @@ async function run() {
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['rust']
           })
           break
@@ -29023,29 +28980,25 @@ async function run() {
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['solidity']
           })
+          break
       }
     }
 
-    /**
-     * Create a comment on the PR with the information we compiled from the
-     * list of changed files.
-     */
     await octokit.rest.issues.createComment({
       owner,
       repo,
-      issue_number: pr_number,
+      issue_number: prNumber,
       body: `
-        Pull Request #${pr_number} has been updated with: \n
-        - ${diffData.changes} changes \n
-        - ${diffData.additions} additions \n
-        - ${diffData.deletions} deletions \n
+        Pull Request #${prNumber} has been updated with:
+        - ${diffData.changes} changes
+        - ${diffData.additions} additions
+        - ${diffData.deletions} deletions
       `
     })
   } catch (error) {
-    // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,56 +1,27 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 async function run() {
   try {
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
     const owner = core.getInput('owner', { required: true })
     const repo = core.getInput('repo', { required: true })
-    const pr_number = core.getInput('pr_number', { required: true })
+    const prNumber = core.getInput('pr_number', { required: true })
     const token = core.getInput('token', { required: true })
 
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
     const octokit = new github.getOctokit(token)
 
-    /**
-     * We need to fetch the list of files that were changes in the Pull Request
-     * and store them in a variable.
-     * We use octokit.paginate() to automatically loop over all the pages of the
-     * results.
-     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-     */
     const { data: changedFiles } = await octokit.rest.pulls.listFiles({
       owner,
       repo,
-      pull_number: pr_number
+      pull_number: prNumber
     })
 
-    /**
-     * Contains the sum of all the additions, deletions, and changes
-     * in all the files in the Pull Request.
-     **/
     let diffData = {
       additions: 0,
       deletions: 0,
       changes: 0
     }
 
-    // Reference for how to use Array.reduce():
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
     diffData = changedFiles.reduce((acc, file) => {
       acc.additions += file.additions
       acc.deletions += file.deletions
@@ -58,21 +29,14 @@ async function run() {
       return acc
     }, diffData)
 
-    /**
-     * Loop over all the files changed in the PR and add labels according
-     * to files types.
-     **/
     for (const file of changedFiles) {
-      /**
-       * Add labels according to file types.
-       */
       const fileExtension = file.filename.split('.').pop()
       switch (fileExtension) {
         case 'md':
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['markdown']
           })
           break
@@ -80,23 +44,16 @@ async function run() {
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['javascript']
           })
           break
         case 'yml':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['yaml']
-          })
-          break
         case 'yaml':
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['yaml']
           })
           break
@@ -104,7 +61,7 @@ async function run() {
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['rust']
           })
           break
@@ -112,29 +69,25 @@ async function run() {
           await octokit.rest.issues.addLabels({
             owner,
             repo,
-            issue_number: pr_number,
+            issue_number: prNumber,
             labels: ['solidity']
           })
+          break
       }
     }
 
-    /**
-     * Create a comment on the PR with the information we compiled from the
-     * list of changed files.
-     */
     await octokit.rest.issues.createComment({
       owner,
       repo,
-      issue_number: pr_number,
+      issue_number: prNumber,
       body: `
-        Pull Request #${pr_number} has been updated with: \n
-        - ${diffData.changes} changes \n
-        - ${diffData.additions} additions \n
-        - ${diffData.deletions} deletions \n
+        Pull Request #${prNumber} has been updated with:
+        - ${diffData.changes} changes
+        - ${diffData.additions} additions
+        - ${diffData.deletions} deletions
       `
     })
   } catch (error) {
-    // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
 }
